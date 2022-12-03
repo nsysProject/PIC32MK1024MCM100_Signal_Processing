0e03bdd13507ea67b642f32cf94058bbc7f6f9cf
nsysProject <debrajdeb123@gmail.com>
2022-12-03 15:42:33 +0530

    comment added to explain use of TMR2 for execution time measurement

--- src/config/pic32mk_mcm_curiosity_pro/interrupts.c
+++ src/config/pic32mk_mcm_curiosity_pro/interrupts.c
@@ -60,7 +60,26 @@
 // *****************************************************************************
 
 void application(void);
-uint16_t TMR2_Count;
+/****************************************************************************
+ * CPU clock = 120MHz
+ * Peripheral clock = CPU/2 = 60MHz
+ * TMR3 configured to trigger ADC at 50KHz
+ * Ignoring interrupt latency, the "max" count for "TMR2_count" should be: 
+ * = 60MHz/50KHz = 1200 = Max count
+ * 
+ * In other words, during code execution if the "TMR2_count" has a value of 1200,
+ * then, the "application" function is taking entire (1/50KHz) = 20uS
+ * 
+ * 1200 count = full 20uS is used by "application"
+ * 
+ * Example-1, if the "TMR2_count" has a value of 300,
+ * then, the "application" function is taking (20/1200)*300 = 5uSec
+ * 
+ * 
+ * Example-2, if the "TMR2_count" has a value of 800,
+ * then, the "application" function is taking (20/1200)*800 = 13.3uSec
+ *****************************************************************************/
+uint16_t TMR2_Count;    // Used to get an idea about execution time
 
 
 
@@ -75,8 +94,8 @@ void __ISR(_ADC_DATA1_VECTOR, ipl1SRS) ADC_DATA1_Handler (void)
 {
     TMR2_CounterClearAndStart();    // Start counter
     application();                  // Application code here
+    TMR2_Count = TMR2_CounterGetAndStop();  // End counter and read    
     X2CScope_Update();              // save value for debugging on X2CScope
-    TMR2_Count = TMR2_CounterGetAndStop();  // End counter and read
     IFS3bits.AD1D1IF = 0;    
 }
 
